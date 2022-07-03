4f08b5189bdb4c290736eaa6324a58cd1ba7bb61
yeji seo <yejiseo0725@gmail.com>
2022-07-03 17:42:03 +0900

    middleware

--- src/server.js
+++ src/server.js
@@ -4,15 +4,15 @@ const PORT = 4000;
 
 const app = express();
 
-const handleHome = (req, res) => {
-  return res.send("I still love you.");
+const gossipMiddleware = (req, res, next) => {
+  console.log(`Someone is trying to go to: ${req.url} now.`);
+  next();
 };
-const handleLogin = (req, res) => {
-  return res.send("Login here :3");
+const handleHome = (req, res) => {
+  return res.send("I love middlewares :3");
 };
 
-app.get("/", handleHome);
-app.get("/login", handleLogin);
+app.get("/", gossipMiddleware, handleHome);
 // app, request get, root, callback function 의 구조
 
 // app.get("/", () => console.log("trying to go home"));
