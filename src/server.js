510f3b29cd6781ddb9c181c85a87309f67ecb3b4
yeji seo <yejiseo0725@gmail.com>
2022-07-05 09:17:37 +0900

    Middleware Recap

--- src/server.js
+++ src/server.js
@@ -4,24 +4,31 @@ const PORT = 4000;
 
 const app = express();
 
-const logger = (req, res, next) => {
-  console.log(`${req.method} ${req.url}`);
-  // method 를 출력하면, get 이 출력된다. (방식)
+// const logger = (req, res, next) => {
+//   console.log(`${req.method} ${req.url}`);
+//   // method 를 출력하면, get 이 출력된다. (방식)
+//   next();
+// };
+
+const routerLogger = (req, res, next) => {
+  console.log(req.path); // 위치 === url
+  next();
+};
+
+const methodLogger = (req, res, next) => {
+  console.log(req.method); // get, post 등의 방식
   next();
 };
 
 const handleHome = (req, res) => {
-  return res.send("I love middlewares :3");
+  return res.send("<h1>Hello!!!</h1>");
 };
 
-app.get("/", logger, handleHome);
+app.get("/", methodLogger, routerLogger, handleHome);
 // app, request get, root, callback function 의 구조
 
 // app.get("/", () => console.log("trying to go home"));
 
-// const handleListening = () => console.log("Server listening on port 4000");
-// app.listen(4000, handleListening);
+const handleListening = () => console.log("Server listening on port 4000");
 
-app.listen(PORT, () =>
-  console.log(`Server listening on port http://localhost:${PORT}`)
-);
+app.listen(4000, handleListening);
